a7acb601621f5be0dcfd8c9efa3cbc20a3657e7e
Martin Sandström <martin@marteinn.se>
2014-11-20 00:02:06 +0100

    Updated description

--- frojd_fabric/__init__.py
+++ frojd_fabric/__init__.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
+"""
+frojd_fabric
+------------
+
+:copyright: (c) 2014 by Fröjd Interactive AB
+:license: MIT, see LICENSE for more details.
+"""
+
+
 __title__ = "frojd_fabric"
 __license__ = "MIT"
+__copyright__ = "Copyright 2014 Fröjd Interactive AB"
 
 
 from .api import (
