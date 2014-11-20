14b2589e112a886fbe56096867b6573be0ccdc8a
Martin Sandström <martin@marteinn.se>
2014-11-20 22:17:17 +0100

    Refactored settings

--- envs/demo.py
+++ envs/demo.py
@@ -6,16 +6,18 @@ from frojd_fabric.utils import get_stage_var
 
 @task
 def demo(default=True):
+    env.run = local
     env.stage = "demo"
-    env.domain = ""
-    env.symlink_name = ""
-    env.hosts = []
+    env.branch = "develop"
+    #env.domain = ""
+    # env.symlink_name = ""
+    #env.hosts = []
     env.user = "root"
     env.password = "password"
-    env.run = local
     env.app_path = get_stage_var("APP_PATH")
     env.source_path = get_stage_var("APP_SOURCE_PATH")
-    env.branch = "develop"
     env.warn_only = True
-    env.requirements = "prod.txt"
     env.max_releases = 5
+
+    # Django
+    env.requirements = "prod.txt"
