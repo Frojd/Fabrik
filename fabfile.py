2e40dbf8714f025903527934fb24da839304078b
Martin Sandström <martin@marteinn.se>
2014-11-20 22:17:32 +0100

    Updated django path

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 import envs
-from envs import demo
+from envs import *
 from frojd_fabric import setup, deploy, rollback
 from frojd_fabric.transfer.git import copy
-from frojd_fabric.ext import django
+from frojd_fabric.recipes import django
