44d2df669ce1f15da6a5f1fe1de09d622561ef08
Martin Sandström <martin@marteinn.se>
2014-11-20 22:17:04 +0100

    Created local env example

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,5 +1,6 @@
 from fabric.state import env
 from demo import demo
+from local import local
 
 
 env.repro_url = ""
---
+++ envs/local.py
@@ -0,0 +1,23 @@
+from fabric.state import env
+from fabric.decorators import task
+from fabric.operations import local
+from frojd_fabric.utils import get_stage_var
+
+
+@task
+def local(default=True):
+    env.run = local
+    env.stage = "demo"
+    env.branch = "develop"
+    #env.domain = ""
+    # env.symlink_name = ""
+    #env.hosts = []
+    #env.user = "root"
+    #env.password = "password"
+    env.app_path = get_stage_var("APP_PATH")
+    env.source_path = get_stage_var("APP_SOURCE_PATH")
+    env.warn_only = True
+    env.max_releases = 5
+
+    # Django
+    env.requirements = "prod.txt"
