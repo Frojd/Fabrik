87b3f5518f1c6c6e18d1934c49ff824ebc4300c7
Martin Sandström <martin@marteinn.se>
2014-11-24 00:28:06 +0100

    Styling and import cleanup

--- fabfile.py
+++ fabfile.py
@@ -2,4 +2,3 @@ import envs
 from envs import *
 from frojd_fabric import setup, deploy, rollback
 from frojd_fabric.transfer.git import copy
-from frojd_fabric.recipes import django
---
+++ frojd_fabric/recipes/django_frojd.py
@@ -0,0 +1,3 @@
+"""
+TODO: Create customized recipe for the frojd demo server
+"""
