e0035192471094a5383abd7ff4670b27d9343c64
Martin Sandström <martin@marteinn.se>
2014-11-23 23:28:35 +0100

    Implemented elocal, updated warning status

--- envs/local_django.py
+++ envs/local_django.py
@@ -2,12 +2,13 @@ from fabric.state import env
 from fabric.decorators import task
 from fabric.operations import local
 from frojd_fabric.utils import get_stage_var
+from frojd_fabric.utils.elocal import elocal
 
 
 
 @task
 def local_django(default=True):
-    env.run = local
+    env.run = elocal
     env.stage = "demo"
     env.branch = "develop"
     # env.domain = ""
@@ -17,7 +18,7 @@ def local_django(default=True):
     env.password = "password"
     env.app_path = get_stage_var("APP_PATH")
     env.source_path = get_stage_var("APP_SOURCE_PATH")
-    env.warn_only = True
+    env.warn_only = False
     env.max_releases = 5
 
     # Django
