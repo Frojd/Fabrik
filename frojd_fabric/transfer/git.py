e7a4577ea7055a62b4a885445ce8f89e5cf49c90
Martin Sandström <martin@marteinn.se>
2014-11-24 19:58:38 +0100

    Added depth 1 to transfer.git. Resolves #4

--- frojd_fabric/transfer/git.py
+++ frojd_fabric/transfer/git.py
@@ -6,7 +6,7 @@ from frojd_fabric.hooks import hook
 @hook("copy")
 def copy():
     with(cd(env.app_path)):
-        env.run("git clone  -b %(branch)s %(repro)s %(path)s" % {
+        env.run("git clone  --depth 1 -b %(branch)s %(repro)s %(path)s" % {
             "branch": env.branch,
             "repro": env.repro_url,
             "path": env.current_release
