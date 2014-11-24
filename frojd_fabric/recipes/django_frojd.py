bf9f78cde5add5be9cee50310698824e396a78c2
Martin Sandström <martin@marteinn.se>
2014-11-24 19:25:53 +0100

    Added setup stub and todo

--- frojd_fabric/recipes/django_frojd.py
+++ frojd_fabric/recipes/django_frojd.py
@@ -3,6 +3,12 @@ from frojd_fabric.ext import uwsgi
 from frojd_fabric.recipes import django
 
 
+@hook("setup")
+def setup():
+    # TODO: Copy .ini file from templates to server
+    pass
+
+
 @hook("after_deploy")
 def after_deploy():
     uwsgi.reload()
