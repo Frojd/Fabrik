1070e26ed3c2c215bdd32466eed31ba8d3210c83
Martin Sandström <martin@marteinn.se>
2014-11-24 17:52:35 +0100

    Moved examples to separate folder

--- examples/envs/local_django.py
+++ examples/envs/local_django.py
@@ -1,13 +1,17 @@
 from fabric.state import env
 from fabric.decorators import task
-from fabric.operations import local
+from fabric.context_managers import lcd
 from frojd_fabric.utils import get_stage_var
+from frojd_fabric.utils.elocal import elocal
 
 
 @task
-def demo(default=True):
-    env.run = local
-    env.stage = "demo"
+def local_django(default=True):
+    from frojd_fabric.recipes import django
+
+    env.run = elocal
+    env.cd = lcd
+    env.stage = "local"
     env.branch = "develop"
     # env.domain = ""
     # env.symlink_name = ""
@@ -16,7 +20,7 @@ def demo(default=True):
     env.password = "password"
     env.app_path = get_stage_var("APP_PATH")
     env.source_path = get_stage_var("APP_SOURCE_PATH")
-    env.warn_only = True
+    env.warn_only = False
     env.max_releases = 5
 
     # Django
