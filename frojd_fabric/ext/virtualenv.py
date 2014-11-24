a60c18baa823c975dbce3c3fbfad7b9838bbcd3e
Martin Sandström <martin@marteinn.se>
2014-11-24 14:14:54 +0100

    Added support for returning path. Added support for hard coded path

--- frojd_fabric/ext/virtualenv.py
+++ frojd_fabric/ext/virtualenv.py
@@ -6,11 +6,24 @@ from unipath import Path
 
 @task
 def create_venv():
-    env.run("virtualenv %s" % paths.get_deploy_path("venv"))
+    if "venv_path" not in env:
+        # logger.error("No venv_path has been specified")
+        return
+
+    env.run("virtualenv %s" % get_path())
 
 
 @task
 def update_requirements():
-    env.run("pip install -r %s" %
-        Path(env.current_release, "requirements", env.requirements),
+    env.run("pip install -r %s" % Path(
+        env.current_release, "requirements", env.requirements),
     )
+
+
+def get_path():
+    if "venv_path" in env:
+        return env.venv_path
+
+    return paths.get_deploy_path("venv")
+
+
