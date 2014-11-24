0387f70bfd2f4e923b028bec959bcccfd120e047
Martin Sandström <martin@marteinn.se>
2014-11-24 14:13:45 +0100

    Created recipe that includes django but adds a uwsgi restart

--- frojd_fabric/recipes/django_frojd.py
+++ frojd_fabric/recipes/django_frojd.py
@@ -1,3 +1,9 @@
-"""
-TODO: Create customized recipe for the frojd demo server
-"""
+from frojd_fabric.hooks import hook
+from frojd_fabric.ext import uwsgi
+from frojd_fabric.recipes import django
+
+
+@hook("after_deploy")
+def after_deploy():
+    uwsgi.reload()
+
