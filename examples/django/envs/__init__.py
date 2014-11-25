8a74fc2514aaffd61e8f1cefff2324d48bbbd08d
Martin Sandström <martin@marteinn.se>
2014-11-25 11:08:24 +0100

    Updated import path

--- examples/django/envs/__init__.py
+++ examples/django/envs/__init__.py
@@ -1,6 +1,6 @@
 from fabric.state import env
 from demo import demo
-from local_django import local_django
+from local import local_django
 
 # Put settings used by all envs here;
 env.repro_url = "git@github.com:Frojd/Si-Fundamentet-Admin.git"
