7db46cbbf83e8b4cd9ea38ccfd5f2daa6a01820f
Martin Sandström <martin@marteinn.se>
2015-03-04 22:50:54 +0100

    Added git branch error handling

--- frojd_fabric/transfer/git.py
+++ frojd_fabric/transfer/git.py
@@ -9,13 +9,22 @@ Sets up a standard git clone procedure.
 from fabric.state import env
 from fabric.context_managers import cd
 from frojd_fabric.hooks import hook
+from ..logger import logger
 
 
 @hook("copy")
 def copy():
+    branch = None
+
+    if "branch" in env:
+        branch = env.branch
+    else:
+        logger.warn("Git branch not set, using master instead")
+        branch = "master"
+
     with(cd(env.app_path)):
         env.run("git clone  --depth 1 -b %(branch)s %(repro)s %(path)s" % {
-            "branch": env.branch,
+            "branch": branch,
             "repro": env.repro_url,
             "path": env.current_release
         })
