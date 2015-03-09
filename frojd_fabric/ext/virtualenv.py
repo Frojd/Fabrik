c177ea70117301ac099b77c7e5ff98476bfa030f
Martin Sandström <martin@marteinn.se>
2015-03-09 23:31:52 +0100

    Added prompt that checks if path already exist. Closes #22

--- frojd_fabric/ext/virtualenv.py
+++ frojd_fabric/ext/virtualenv.py
@@ -6,16 +6,27 @@ frojd_fabric.ext.virtualenv
 Holds methods for managing virtualenv.
 """
 
-from fabric.decorators import task
-from fabric.state import env
-from frojd_fabric import paths
 from unipath import Path
+from fabric.decorators import task
+from fabric.operations import prompt
+from fabric.state import env
+
+from frojd_fabric import paths
+from ..logger import logger
 
 
 @task
 def create_venv():
     if "venv_path" not in env:
         raise Exception("No env.venv_path has been specified")
+
+    # Checks if venv exist and prompts user if reinstalling is an option
+    if env.exists(Path(get_path(), "bin")):
+        logger.warn("Virtualenv is already installed")
+        prompt_result = prompt("Install anyways:", default="no")
+
+        if prompt_result == "no":
+            return
 
     env.run("virtualenv %s" % get_path())
 
