8c5fb6b66b96e92b4db09044fabe978b3a2ac387
Martin Sandström <martin@marteinn.se>
2015-04-29 21:07:49 +0200

    Updated hook function argument names

--- frojd_fabric/hooks.py
+++ frojd_fabric/hooks.py
@@ -13,78 +13,78 @@ import inspect
 _hooks = {}
 
 
-def hook(hook_name=None, priority=-1):
+def hook(name=None, priority=-1):
     """
     Decorator
     """
 
-    def _hook(view_func):
-        return register_hook(hook_name, view_func=view_func, priority=priority)
+    def _hook(hook_func):
+        return register_hook(name, hook_func=hook_func, priority=priority)
 
     return _hook
 
 
-def register_hook(hook_name, view_func=None, priority=-1):
-    if hook_name not in _hooks:
-        _hooks[hook_name] = []
+def register_hook(name, hook_func=None, priority=-1):
+    if name not in _hooks:
+        _hooks[name] = []
 
-    hook_list = _hooks[hook_name]
+    hook_list = _hooks[name]
 
     if priority == -1:
         priority = len(hook_list)
 
     def _decorator(*args, **kwargs):
-        return view_func(*args, **kwargs)
+        return hook_func(*args, **kwargs)
 
-    wrap = wraps(view_func)(_decorator)
-    wrap.hook_id = _get_hook_id(view_func)
+    wrap = wraps(hook_func)(_decorator)
+    wrap.hook_id = _get_hook_id(hook_func)
 
     hook_list.insert(min(len(hook_list), priority), wrap)
 
     return wrap
 
 
-def _get_hook_id(view_func):
-    package_name = inspect.getmodule(view_func).__name__
-    full_path = "%s.%s" % (package_name, view_func.__name__)
+def _get_hook_id(hook_func):
+    package_name = inspect.getmodule(hook_func).__name__
+    full_path = "%s.%s" % (package_name, hook_func.__name__)
 
     return full_path
 
 
-def run_hook(hook_name, *args, **kwargs):
-    if not has_hook(hook_name):
+def run_hook(name, *args, **kwargs):
+    if not has_hook(name):
         return
 
-    hook_list = _hooks[hook_name]
-    for hook_function in hook_list:
-        hook_function(*args, **kwargs)
+    hook_list = _hooks[name]
+    for function in hook_list:
+        function(*args, **kwargs)
 
 
-def has_hook(hook_name, view_func=None):
-    if hook_name not in _hooks:
+def has_hook(name, hook_func=None):
+    if name not in _hooks:
         return False
 
-    if len(_hooks[hook_name]) == 0:
+    if len(_hooks[name]) == 0:
         return False
 
-    if not view_func:
+    if not hook_func:
         return True
 
-    for hook_func in _hooks[hook_name]:
-        hook_id = _get_hook_id(view_func)
+    for func in _hooks[name]:
+        hook_id = _get_hook_id(hook_func)
 
-        if hook_id == hook_func.hook_id:
+        if hook_id == func.hook_id:
             return True
 
     return False
 
 
-def unregister_hook(hook_name, view_func=None):
-    if not has_hook(hook_name, view_func=view_func):
+def unregister_hook(name, hook_func=None):
+    if not has_hook(name, hook_func=hook_func):
         return
 
-    hook_id = _get_hook_id(view_func)
+    hook_id = _get_hook_id(hook_func)
 
-    for hook_func in _hooks[hook_name][:]:
-        if hook_id == hook_func.hook_id:
-            _hooks[hook_name].remove(hook_func)
+    for func in _hooks[name][:]:
+        if hook_id == func.hook_id:
+            _hooks[name].remove(func)
