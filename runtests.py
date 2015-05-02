0e3d1c6a5ae49281e74001537389b252eaa1ba6e
Martin Sandström <martin@marteinn.se>
2015-05-02 07:15:39 +0200

    Added support for specifiying unittests

--- runtests.py
+++ runtests.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 from fabric.state import env
 from fabric.state import output
 
 
-def runtests():
+def prepare_test():
     import logging
     from frojd_fabric.logger import logger
-
-    # List of modules to test
-    testmodules = [
-        "tests.test_api"
-    ]
 
     # Mute fabric
     output["status"] = False
@@ -30,10 +26,20 @@ def runtests():
     # Disable frojd_fabric logging
     logger.setLevel(logging.CRITICAL)
 
+
+def runtests(test_modules=None):
+    prepare_test()
+
+    # List of modules to test
+    if (not test_modules):
+        testmodules = [
+            "tests.test_api"
+        ]
+
     # Construct and run test suite
     suite = unittest.TestSuite()
 
-    for t in testmodules:
+    for t in test_modules:
         try:
             mod = __import__(t, globals(), locals(), ["suite"])
             suitefn = getattr(mod, "suite")
@@ -45,4 +51,9 @@ def runtests():
 
 
 if __name__ == "__main__":
-    runtests()
+    test_modules = None
+
+    if len(sys.argv) > 1:
+        test_modules = [sys.argv[1]]
+
+    runtests(test_modules)
