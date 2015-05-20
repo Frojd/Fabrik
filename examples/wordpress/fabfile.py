80d6ad2644dbe06ff116f1a826b7bd7d69039e80
Martin Sandström <martin@marteinn.se>
2015-05-20 19:47:16 +0200

    Moved git import to stages/__init__ for wp

--- examples/wordpress/fabfile.py
+++ examples/wordpress/fabfile.py
@@ -1,4 +1,3 @@
 from stages import *
 from frojd_fabric.api import setup, deploy, rollback, debug
-from frojd_fabric.transfer.git import copy
 
--- examples/wordpress/stages/__init__.py
+++ examples/wordpress/stages/__init__.py
@@ -1,5 +1,8 @@
 from fabric.state import env
 from local import local
 
+from frojd_fabric.transfer.git import copy
+
+
 # Put settings used by all env stages
 env.repro_url = "git@github.com:Frojd/Yourrepro.git"
