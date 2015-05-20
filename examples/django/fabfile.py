848d47cda3a3e66f3da695eb1fbfe2ec673423fc
Martin Sandström <martin@marteinn.se>
2015-05-20 19:45:10 +0200

    Renamed stages paths. Moved git import to stages/__init__.py

--- examples/django/fabfile.py
+++ examples/django/fabfile.py
@@ -1,3 +1,2 @@
-from envs import *
+from stages import *
 from frojd_fabric.api import setup, deploy, rollback, debug
-from frojd_fabric.transfer.git import copy
--- examples/django/stages/__init__.py
+++ examples/django/stages/__init__.py
@@ -4,7 +4,8 @@ from demo import demo
 from local import local
 from stage import stage
 
+from frojd_fabric.transfer.git import copy
+
 
 # Put settings used by all env stages
 env.repro_url = "git@github.com:Frojd/Yourrepro.git"
-
