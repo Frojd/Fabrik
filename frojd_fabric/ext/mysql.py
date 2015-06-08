3f2e79c67e22b4fe6c4de704e8b3518d367b5183
Martin Sandström <martin@marteinn.se>
2015-06-08 23:34:37 +0200

    Added mysql extension

---
+++ frojd_fabric/ext/mysql.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+frojd_fabric.ext.mysql
+----------------------
+Mysql extension that handles backup and restore (future).
+
+Params:
+    env.mysql_user
+    env.mysql_password
+    env.mysql_db
+"""
+
+from fabric.decorators import task
+from fabric.state import env
+from frojd_fabric import paths
+
+
+@task
+def backup_db(limit=5):
+    current_release = paths.get_current_release_name()
+
+    max_versions = limit+1
+
+    if not current_release:
+        return
+
+    env.run("mkdir -p %s" % paths.get_backup_path("mysql"))
+
+    backup_file = "mysql/%s.sql" % current_release
+    backup_path = paths.get_backup_path(backup_file)
+
+    env.run("mysqldump -u %s -p%s %s > %s" %
+            (env.mysql_user, env.mysql_password, env.mysql_db, backup_path))
+
+    # Remove older releases
+    env.run("ls -dt %s/* | tail -n +%s | xargs rm -rf" % (
+        paths.get_backup_path("mysql"),
+        max_versions)
+    )
+
