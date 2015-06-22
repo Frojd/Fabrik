e8347be627397dadb3e6298b4a6b52ec968b34c9
Martin Sandström <martin@marteinn.se>
2015-06-22 11:23:10 +0200

    Made path an argument to the composer methods

--- frojd_fabric/ext/composer.py
+++ frojd_fabric/ext/composer.py
@@ -12,17 +12,19 @@ from frojd_fabric import paths
 
 
 @task
-def install():
-    release_path = paths.get_current_release_path()
+def install(install_path=None):
+    if not install_path:
+        install_path = paths.get_current_release_path()
 
-    with(env.cd(release_path)):
+    with(env.cd(install_path)):
         env.run("curl -sS https://getcomposer.org/installer | php")
 
 
 @task
-def update():
-    release_path = paths.get_current_release_path()
+def update(install_path=None):
+    if not install_path:
+        install_path = paths.get_current_release_path()
 
-    with(env.cd(release_path)):
+    with(env.cd(install_path)):
         env.run("php composer.phar install")
 
