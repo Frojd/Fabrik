ec26fe9f3fa079b7837c95fa8447bfbc4d2f7e01
Martin Sandström <martin@marteinn.se>
2016-12-02 15:43:15 +0100

    Updated scp task to support ignore list and local path resolve

--- fabrik/transfer/scp.py
+++ fabrik/transfer/scp.py
@@ -4,29 +4,57 @@
 fabrik.transfer.scp
 -------------------------
 Transfer release by scp
+
+Params:
+    env.local_app_path: Manually defined path to local app path
+    env.scp_ignore_list: A list of paths that should be ignored on sync
 """
 
 from fabric.state import env
 from fabric.operations import local, put
-from fabric.context_managers import settings, lcd, cd
+from fabric.context_managers import lcd, cd
 
+from fabrik import paths
 from fabrik.hooks import hook
-from fabrik.logger import logger
+from fabrik.utils.elocal import elocal
 
-ignore_list = []
+
+def get_local_app_path():
+    if 'local_app_path' in env:
+        return env.local_app_path
+
+    path = elocal('git rev-parse --show-toplevel', capture=True)
+    return path
 
 
 @hook("copy")
 def copy():
-    env.run('mkdir ' + env.current_release)
+    default_ignore_list = ['build.tar.gz', ]
+    ignore_list = []
 
-    with lcd('..'), cd(env.current_release):
-        build_filename = "build.tar.gz"
+    if 'scp_ignore_list' in env:
+        ignore_list = env.scp_ignore_list
+
+    ignore_list = ignore_list + default_ignore_list
+
+    path = get_local_app_path()
+    release_path = paths.get_deploy_path(env.current_release)
+
+    env.run('mkdir -p {}'.format(release_path))
+
+    with lcd(path), cd(release_path):
+        build_filename = 'build.tar.gz'
         build_remote_path = "/".join([env.current_release, build_filename])
 
-        local("ls | egrep -v 'build.tar.gz|git-hooks|scripts|deploy|docker' | xargs tar -zcf " + build_filename)
-        put(build_filename, build_remote_path)
-        env.run("tar -xzf " + build_remote_path)
+        exclude_args = map(lambda x: '--exclude="{}"'.format(x), ignore_list)
 
-        env.run("rm " + build_remote_path)
+        local('tar {} -czf {} *'.format(
+            ' '.join(exclude_args),
+            build_filename
+        ))
+
+        put(build_filename, build_remote_path)
+        env.run('tar -xzf {}'.format(build_filename))
+
+        env.run('rm {}'.format(build_remote_path))
         local("rm build.tar.gz")
