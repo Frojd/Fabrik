3f4cec1d1a7cdf9d19fcdd135122654c913f3412
Martin Sandström <martin@marteinn.se>
2017-01-13 12:13:23 +0100

    Bumped version and license year

--- fabrik/__init__.py
+++ fabrik/__init__.py
@@ -5,13 +5,13 @@
 fabrik
 ------------
 
-:copyright: (c) 2014-2016 by Fröjd Interactive AB
+:copyright: (c) 2014-2017 by Fröjd Interactive AB
 :license: MIT, see LICENSE for more details.
 """
 
 
 __title__ = "fabrik"
-__version__ = "3.1.0"
-__build__ = 310
+__version__ = "3.2.0"
+__build__ = 320
 __license__ = "MIT"
-__copyright__ = "Copyright 2014-2016 Fröjd Interactive AB"
+__copyright__ = "Copyright 2014-2017 Fröjd Interactive AB"
